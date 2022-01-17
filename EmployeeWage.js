f153a0c0daad1aaba01885a477427ef6e2248452
Navneet <navn0306@gmail.com>
2022-01-18 01:10:15 +0530

    ADDED : Ability to calculate employee daily wage.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,27 @@
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is Absent");
-} else {
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHrs = 0;
 
-    console.log("Employee is Present");
+//To check Employee hours.
+function empCheck() {
+    let check = Math.floor(Math.random() * 10) % 3;
+    return check;
 }
 
+switch (empCheck()) {
+    case 1:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case 2:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
+}
+
+//To calculate Employee Daily wage.
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Today's Employee Wage:" + empWage);
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 UseCase 1 -
 Ability to Check Employee is present or Absent.
+
+UseCase 2 -
+Ability to Calculate Daily Employee Wage based on part time or full time work.
